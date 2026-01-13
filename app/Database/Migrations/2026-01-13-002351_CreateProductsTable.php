223f4529a0095eed42272f2aed24041df9809634
Katon Surya Atmaja <katonatmaja@gmail.com>
2026-01-13 07:26:17 +0700

    create models and migrations
    - create user model
    - create product model
    - create users table migration
    - create products table migration

---
+++ app/Database/Migrations/2026-01-13-002351_CreateProductsTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'price' => [
+                'type' => 'INT',
+            ],
+            'stock' => [
+                'type' => 'INT',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('products');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('products');
+    }
+}
